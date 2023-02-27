df042d545a875c005a6e2d6cac5f5ff3a9536a83
atchim <atchim.gama.silva@gmail.com>
2023-02-27 06:29:31 -0300

    refactor(qutebrowser): enhance configurations

---
+++ qutebrowser/bindings.py
@@ -0,0 +1,18 @@
+from typing import Any, TYPE_CHECKING
+if TYPE_CHECKING:
+  config = Any
+
+# Tab Switching
+config.bind("<Ctrl+Tab>", "tab-focus stack-prev")
+config.bind("<Ctrl+Shift+Tab>", "tab-focus stack-next")
+
+# Zoom
+config.unbind("+")
+config.bind("z+", "zoom-in")
+config.unbind("-")
+config.bind("z-", "zoom-out")
+config.unbind("=")
+config.bind("zz", "zoom")
+
+# Misc
+config.bind('tt', 'set-cmd-text -s :tab-take')
--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,180 +1,39 @@
-# Sopa de Mamaco's Palette
-p = [
-  '#080808',
-  '#0c0c0c',
-  '#121212',
-  '#1a1a1a',
-  '#252525',
-  '#383838',
-  '#5a5a5a',
-  '#959595',
-  '#ff5b5b',
-  '#bc8f05',
-  '#54a900',
-  '#00ad2b',
-  '#00a7a7',
-  '#6b90ff',
-  '#ba74ff',
-  '#fc4ad0',
-]
+from typing import TYPE_CHECKING, Any
 
-# Colors {{{1
+import tomli
 
-c.colors.completion.category.bg = p[1]
-c.colors.completion.category.fg = p[7]
-c.colors.completion.category.border.bottom = p[1]
-c.colors.completion.category.border.top = p[1]
-c.colors.completion.even.bg = p[0]
-c.colors.completion.fg = [p[7], p[6], p[10]]
-c.colors.completion.item.selected.bg = p[2]
-c.colors.completion.item.selected.border.bottom = p[2]
-c.colors.completion.item.selected.border.top = p[2]
-c.colors.completion.item.selected.fg = p[7]
-c.colors.completion.item.selected.match.fg = p[10]
-c.colors.completion.match.fg = p[14]
-c.colors.completion.odd.bg = p[0]
-c.colors.completion.scrollbar.bg = p[0]
-c.colors.completion.scrollbar.fg = p[4]
+if TYPE_CHECKING:
+  config = Any
 
-c.colors.contextmenu.disabled.bg = p[0]
-c.colors.contextmenu.disabled.fg = p[6]
-c.colors.contextmenu.menu.bg = p[0]
-c.colors.contextmenu.menu.fg = p[7]
-c.colors.contextmenu.selected.bg = p[1]
-c.colors.contextmenu.selected.fg = p[7]
-
-c.colors.downloads.bar.bg = p[0]
-c.colors.downloads.start.fg = p[0]
-c.colors.downloads.start.bg = p[10]
-c.colors.downloads.stop.fg = p[0]
-c.colors.downloads.stop.bg = p[11]
-c.colors.downloads.error.fg = p[0]
-c.colors.downloads.error.bg = p[8]
-
-c.colors.hints.bg = p[14]
-c.colors.hints.fg = p[0]
-c.colors.hints.match.fg = p[6]
-
-c.colors.keyhint.bg = p[0]
-c.colors.keyhint.fg = p[7]
-c.colors.keyhint.suffix.fg = p[10]
-
-c.colors.messages.error.bg = p[8]
-c.colors.messages.error.border = p[8]
-c.colors.messages.error.fg = p[0]
-c.colors.messages.info.bg = p[11]
-c.colors.messages.info.border = p[11]
-c.colors.messages.info.fg = p[0]
-c.colors.messages.warning.bg = p[10]
-c.colors.messages.warning.border = p[10]
-c.colors.messages.warning.fg = p[0]
-
-c.colors.prompts.bg = p[0]
-c.colors.prompts.border = p[0]
-c.colors.prompts.fg = p[7]
-c.colors.prompts.selected.bg = p[1]
-
-c.colors.statusbar.caret.bg = p[0]
-c.colors.statusbar.caret.fg = p[7]
-c.colors.statusbar.caret.selection.bg = p[0]
-c.colors.statusbar.caret.selection.fg = p[7]
-c.colors.statusbar.command.bg = p[0]
-c.colors.statusbar.command.fg = p[7]
-c.colors.statusbar.command.private.bg = p[0]
-c.colors.statusbar.command.private.fg = p[7]
-c.colors.statusbar.insert.bg = p[0]
-c.colors.statusbar.insert.fg = p[7]
-c.colors.statusbar.normal.bg = p[0]
-c.colors.statusbar.normal.fg = p[7]
-c.colors.statusbar.passthrough.bg = p[0]
-c.colors.statusbar.passthrough.fg = p[7]
-c.colors.statusbar.private.bg = p[0]
-c.colors.statusbar.private.fg = p[6]
-c.colors.statusbar.progress.bg = p[7]
-c.colors.statusbar.url.error.fg = p[8]
-c.colors.statusbar.url.fg = p[7]
-c.colors.statusbar.url.hover.fg = p[14]
-c.colors.statusbar.url.success.http.fg = p[7]
-c.colors.statusbar.url.success.https.fg = p[11]
-c.colors.statusbar.url.warn.fg = p[10]
-
-c.colors.tabs.bar.bg = p[1]
-c.colors.tabs.even.bg = p[0]
-c.colors.tabs.even.fg = p[6]
-c.colors.tabs.indicator.error = p[8]
-c.colors.tabs.indicator.start = p[10]
-c.colors.tabs.indicator.stop = p[11]
-c.colors.tabs.odd.bg = p[0]
-c.colors.tabs.odd.fg = p[6]
-c.colors.tabs.pinned.even.bg = p[1]
-c.colors.tabs.pinned.even.fg = p[6]
-c.colors.tabs.pinned.odd.bg = p[1]
-c.colors.tabs.pinned.odd.fg = p[6]
-c.colors.tabs.pinned.selected.even.bg = p[0]
-c.colors.tabs.pinned.selected.even.fg = p[7]
-c.colors.tabs.pinned.selected.odd.bg = p[0]
-c.colors.tabs.pinned.selected.odd.fg = p[7]
-c.colors.tabs.selected.even.bg = p[0]
-c.colors.tabs.selected.even.fg = p[7]
-c.colors.tabs.selected.odd.bg = p[0]
-c.colors.tabs.selected.odd.fg = p[7]
-
-# Completion {{{1
-c.completion.cmd_history_max_items = 128
-c.completion.scrollbar.padding = 0
-c.completion.scrollbar.width = 16
-c.completion.shrink = True
-
-# Downloads {{{1
-c.downloads.location.directory = '~/dl'
-c.downloads.location.prompt = False
-c.downloads.location.remember = False
-c.downloads.location.suggestion = 'filename'
-c.downloads.position = 'bottom'
-c.downloads.remove_finished = 0
-
-# Hints {{{1
-c.hints.border = '0px solid'
-c.hints.radius = 0
-
-# Tabs {{{1
-c.tabs.new_position.unrelated = 'next'
-c.tabs.padding = {'bottom': 0, 'left': 0, 'right': 0, 'top': 0}
-c.tabs.position = 'left'
-c.tabs.select_on_remove = 'last-used'
-c.tabs.show = 'never'
-c.tabs.show_switching_delay = 500
-c.tabs.title.format = '{audio}{index} {current_title}'
-c.tabs.width = 64
-
-# URL {{{1
-c.url.default_page = 'https://www.google.com'
-c.url.searchengines = {
-  'DEFAULT': 'https://google.com/search?q={}',
-  'a': 'https://wiki.archlinux.org/index.php?search={}',
-  'd': 'https://duckduckgo.com/?q={}',
-  'dpl': 'https://www.deepl.com/translator#au/au/{}',
-  'g': 'https://www.google.com/search?q={}',
-  'gb': 'https://gentoobrowse.randomdan.homeip.net/search?criteria={}',
-  'gbp': 'https://gentoobrowse.randomdan.homeip.net/packages/{}',
-  'gh': 'https://github.com/search?q={}',
-  'gh/': 'https://github.com/{}',
-  'gt': 'https://translate.google.com/?sl=auto&text={}',
-  'rs': 'https://doc.rust-lang.org/std/index.html?search={}',
-  'rsc': 'https://docs.rs/releases/search?query={}',
-  'sc': 'https://soundcloud.com/search?q={}',
-  'w': 'https://wikipedia.org/w/index.php?search={}',
-  'y': 'https://www.youtube.com/results?search_query={}',
-  'zu': 'http://gpo.zugaina.org/Search?search={}',
-}
-c.url.start_pages = 'https://www.google.com'
-
-# Misc {{{1
-c.auto_save.session = True
-c.fonts.default_family = 'Hack Nerd Font'
-c.keyhint.radius = 0
-c.prompt.radius = 0
-c.spellcheck.languages = ['en-US', 'pt-BR']
-
-# Bottom {{{1
 config.load_autoconfig(False)
+config.source('bindings.py')
+
+def dict_attrs(obj, path=''):
+  if isinstance(obj, dict):
+    for k, v in obj.items():
+      # WORKAROUND: For settings that requires a `dict` as value.
+      if (
+        path == 'tabs' and k == 'padding'
+        or path == 'url' and k == 'searchengines'
+      ):
+        yield f'{path}.{k}', v
+      else:
+        yield from dict_attrs(v, f'{path}.{k}' if path else k)
+  else:
+    yield path, obj
+
+def set_c():
+  with open(config.configdir / 'c.toml', 'rb') as f:
+    data = tomli.load(f)
+
+  palette = data['colors'].pop('_palette')
+
+  for k, v in dict_attrs(data):
+    if k.startswith('colors.'):
+      if isinstance(v, list):
+        v = [palette[i] for i in v]
+      else:
+        v = palette[v]
+    config.set(k, v)
+
+set_c()
